a3facb6758d09c38c81c75aed26ef073bd49568d
BloodyCrown <3170989534@qq.com>
2026-02-19 20:25:43 +0800

    feat(database): 添加数据库表格查看按钮功能
    - 新增 ExtraMesButtons 组件用于显示数据库表格查看按钮
    - 实现点击事件监听器绑定到文档级别的 .open_database_table 元素
    - 使用 Teleport 将按钮渲染到 .extraMesButtons 容器中
    - 在组件卸载时正确移除事件监听器避免内存泄漏
    - 在 global.d.ts 中声明全局 $ 类型定义

--- global.d.ts
+++ global.d.ts
@@ -14,4 +14,5 @@ declare global {
             saveSettingsDebounced: () => void;
         };
     };
+    const $:any;
 }
---
+++ src/app/components/pure-components/ExtraMesButtons.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import {onMounted, onUnmounted} from 'vue'
+
+const emit = defineEmits(['click'])
+
+function handleClick() {
+  emit('click')
+}
+
+onMounted(() => {
+  $(document).on('click', '.open_database_table', handleClick)
+})
+
+onUnmounted(() => {
+  $(document).off('click', '.open_database_table', handleClick)
+})
+</script>
+
+<template>
+  <Teleport to=".extraMesButtons">
+    <div class="mes_button open_database_table" title="查看数据库表格">
+      <i class="fa-solid fa-table"></i>
+    </div>
+  </Teleport>
+</template>
